c9ddb43cee36fd24c73a058b23748a80b246049a
Azeez <azeezsulaiman05@gmail.com>
2025-11-03 08:16:49 -0800

    Change case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Courses from "./pages/courses";
+import Courses from "./pages/Courses";
 
 function App() {
   return (
